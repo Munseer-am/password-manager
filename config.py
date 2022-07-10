e14525cfecccc103f15749bd2d60349219443512
Munseer-am <munnamunseer7@gmail.com>
2022-07-10 19:44:27 +0530

    config

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-config = {
-    'KEY': 'e7cc63069e51651c3a48828ed5e467ee',
-    'PATH_TO_DATABASE': '/home/munseer/code/database/db.sqlite3',
-    'PATH_TO_LOG': '/home/munseer/code/python/Log/'
-}
+config = None
