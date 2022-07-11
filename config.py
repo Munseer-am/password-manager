91fe4ae962524207f0e03f5dc67827788013ffd1
Munseer-am <82161602+Munseer-am@users.noreply.github.com>
2022-07-11 12:53:12 +0530

    Create config.py

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-config = {
-    'KEY': 'b9cacc7104194d876db6d3083aba914f',
-    'PATH_TO_DATABASE': '/home/munseer/code/database/db.sqlite3',
-    'PATH_TO_LOG': '/home/munseer/code/python/Log'
-}
+config = None
