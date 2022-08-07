c7c3b739644dfad7927182c052aa0b6b5b779a7a
Munseer-am <munnamunseer7@gmail.com>
2022-08-07 16:41:58 +0530

    script stuck in loop

--- install.sh
+++ install.sh
@@ -1,5 +1,29 @@
 #! /bin/bash
 
+if [ $(grep '^ID_LIKE' /etc/os-release) == "ID_LIKE=arch" ]; then
+    if ! [ -x "$(`command -v figlet`)" ]; then
+        echo "Error: figlet is not installed"
+        echo "Installing figlet..."
+        sudo pacman -Sy figlet
+    fi
+    if ! [ -x "$(`command -v lolcat`)" ]; then
+        echo "Error: lolcat is not installed"
+        echo "Installing figlet..."
+        sudo pacman -Sy lolcat
+    fi
+else
+    if ! [ -x "$(`command -v figlet`)" ]; then
+        echo "Error: figlet is not installed"
+        echo "Installing figlet..."
+        sudo apt install figlet
+    fi
+    if ! [ -x "$(`command -v lolcat`)" ]; then
+        echo "Error: lolcat is not installed"
+        echo "Installing figlet..."
+        sudo apt install lolcat
+    fi
+fi
+
 function install () {
     echo "Installing Script"
     sleep 1;
@@ -12,51 +36,18 @@ function install () {
     echo "To run the script type manager"
 }
 
-if [ $(grep '^ID_LIKE' /etc/os-release) == "ID_LIKE=arch" ]; then
-	if ! [ -x "$(`command -v figlet`)" ]; then
-		echo "Error: figlet is not installed." >&2
-		echo "Installing figlet"
-		sudo pacman -Sy figlet
-	else
-		:
-
-	fi
-	if ! [ -x "$(`command -v lolcat`)" ]; then
-	  echo "Error: lolcat is not installed." >&2
-	  echo "Installing lolcat"
-	  sudo pacman -Sy lolcat
-	else
-	  :
-	fi
-else
-	if ! [ -x "$(`command -v figlet`)" ]; then
-		echo "Error: figlet is not installed." >&2
-		echo "Installing figlet"
-		sudo apt-get install figlet
-	else
-		:
-	fi
-	if ! [ -x "$(`command -v lolcat`)" ];then
-	  echo "Error: lolcat is not installed." >&2
-	  echo "Installing lolcat"
-	  sudo apt-get install lolcat
-	else
-	  :
-	fi
-fi
-
-# clear
 cp Bloody.flf /usr/share/figlet/fonts/
 
 figlet -c -f Bloody "Munseer" | lolcat
 
 FILE=/usr/local/bin/manager
 if [[ -f $FILE ]]; then
-	echo "Script Already Installed"
-	exit
+    echo "Script Already Installed"
+    exit
 else
     mkdir ~/.config/manager/
     mkdir ~/.config/manager/log
     mkdir ~/.config/manager/backup/
     install
 fi
+
