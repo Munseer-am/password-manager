c2ffcf1b0d52efcb5b417f7ee442dc248eb13ea1
Munseer-am <munnamunseer7@gmail.com>
2022-08-11 10:33:49 +0530

    Fixed overriding error in install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ function tool() {
             echo "Installing figlet..."
             sudo pacman -Sy figlet
         else
-        echo "Figlet is installed"
+            :
         fi
         if ! [ -x "$(`command -v lolcat`)" ]; then
             echo "Error: lolcat is not installed"
@@ -21,11 +21,15 @@ function tool() {
             echo "Error: figlet is not installed"
             echo "Installing figlet..."
             sudo apt install figlet
+        else 
+            :
         fi
         if ! [ -x "$(`command -v lolcat`)" ]; then
             echo "Error: lolcat is not installed"
             echo "Installing figlet..."
             sudo apt install lolcat
+        else
+            :
         fi
     fi
 }
@@ -35,14 +39,10 @@ function install () {
     sleep 1;
     echo "Moving Files"
     sudo cp manager.py /usr/local/bin/manager
-    cp config.py menu.py /home/munseer/.config/manager
-    touch ~/.config/manager/db.sqlite3
-    echo "Changing Permission"
-    sudo chmod +x /usr/local/bin/manager
     echo "To run the script type manager"
 }
 
-sudo cp Bloody.flf /usr/share/figlet/fonts/
+# sudo cp Bloody.flf /usr/share/figlet/fonts/
 
 figlet -c -f Bloody "Munseer" | lolcat
 
@@ -51,10 +51,35 @@ if [[ -f $FILE ]]; then
     echo "Script Already Installed"
     exit
 else
-    mkdir ~/.config/manager/
-    mkdir ~/.config/manager/log
-    mkdir ~/.config/manager/backup/
+    FILE=~/.config/manager/
+    if [ -d $FILE ];then
+        echo "Directory $FILE Exists"
+    else
+        mkdir $FILE
+    fi
+    FILE1=~/.config/manager/log
+    if [ -d $FILE1 ];then
+        echo "Directory $FILE1 Exists"
+    else
+        mkdir $FILE1
+    fi 
+    FILE2=~/.config/manager/backup
+    if [ -d $FILE2 ];then
+        echo "Directory $FILE2 Exists"
+    else
+        mkdir $FILE2
+    fi
+    if [ -f ~/.config/manager/config.py ];then
+        echo "Found Existing configuration"
+    else
+        cp config.py ~/.config/manager/
+    fi
+    if [ -f ~/.config/manager/db.sqlite3 ];then
+        echo "Found Existing Database"
+    else
+        touch ~/.config/manager/db.sqlite3
+    fi
     install
-    tool
+    # tool
 fi
 
