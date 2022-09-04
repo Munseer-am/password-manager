73c822c8cf6355ade121390ae76c9d0ca1a0a0e4
Munseer-am <munnamunseer7@gmail.com>
2022-09-04 09:26:16 +0530

    added set_details function

--- manager.py
+++ manager.py
@@ -1,5 +1,9 @@
+#! /usr/bin/env python3
+import argparse
 import clipboard
 import os
+# import re
+import sqlite3
 import sys
 from cryptography.fernet import Fernet
 from functools import cache
@@ -13,6 +17,11 @@ from string import ascii_lowercase, ascii_uppercase, digits
 sys.path.insert(0, os.path.join(os.path.expanduser("~") + "/.config/manager/"))
 from config import config
 from menu import menu
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--reset", help="reset the password of script")
+parser.add_argument("-U", "--uninstall", help="Delete script file from /usr/local/bin/")
+args = parser.parse_args()
 
 console = Console()
 
@@ -38,7 +47,7 @@ def copy(word: str):
 
 
 @cache
-def is_configured():
+def is_not_configured():
     if config is not None:
         return False
     else:
@@ -70,3 +79,58 @@ def decrypt(key: bytes, password: bytes):
 @cache
 def backup(db: str, path: str, dst: str):
     copyfile(path, os.path.join(dst + "/" + db))
+
+
+class Main:
+    def __init__(self):
+        super(Main, self).__init__()
+        self.home = os.path.expanduser("~")
+        self.conn = sqlite3.connect(os.path.join(self.home + "/.config/manager/db.sqlite3"))
+        self.cur = self.conn.cursor()
+        if is_not_configured():
+            self.set_details()
+
+    def set_details(self):
+        tables = """CREATE TABLE IF NOT EXISTS Passwords (
+            Application VARCHAR(200),
+            Username VARCHAR(200),
+            Email VARCHAR(200),
+            Password VARCHAR(200)
+        );"""
+        log = """CREATE TABLE IF NOT EXISTS Log (
+            App VARCHAR(100),
+            Time VARCHAR(100),
+            Script VARCHAR(100)
+        );"""
+        self.cur.execute(tables)
+        self.cur.execute(log)
+        self.conn.commit()
+        self.conn.close()
+        master = Prompt.ask("Set a master password to use", password=True)
+        val = Prompt.ask("Enter password again", password=True)
+        if master != val:
+            console.print("[bold]Password does not match[/bold]")
+            self.set_details()
+        email = Prompt.ask("Enter your email address(It will be used to reset password)")
+        if "@" not in email:
+            console.print("Enter a valid email address")
+            self.set_details()
+        email = sha256_encoder(email)
+        key = Fernet.generate_key()
+        enc = sha256_encoder(master)
+        conf = f"""{{
+    'KEY': '{enc},
+    'ENCRYPTION_KEY': '{key}',
+    'EMAIL': '{email}',
+    'PATH_TO_DATABASE': '{os.path.join(self.home + "/.config/manager/db.sqlite3")}',
+    'PATH_TO_BACKUP': '{os.path.join(self.home + "/.config/manager/backup/")}',
+    'PATH_TO_LOG': '{os.path.join(self.home + "/.config/manager/log/")}'            
+}}"""
+        with open(os.path.join(self.home + "/.config/manager/config.py") , "w") as f:
+            f.write(conf)
+            f.close()
+        backup(config.bak, os.path.join(self.home + "/.config/manager/config.py"), os.path.join(self.home + "/.config/manager/backup"))
+        console.print("Please run the script again")
+        quit(0)
+
+
