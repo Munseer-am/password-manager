cc911e01f8bab84611aec02461760ddf1b9931b7
Munseer-am <munnamunseer7@gmail.com>
2022-09-23 10:04:31 +0530

    Script completed

--- install.cpp
+++ install.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <filesystem>
+#include <unistd.h>
+#include <sys/types.h>
+#include <bits/stdc++.h>
+#include <pwd.h>
 using namespace std;
 
 int is_installed(string tool) {
@@ -48,23 +52,55 @@ int tool() {
 }
 
 int main() {
+    struct passwd *pw = getpwuid(getuid());
+    const char *homedir = pw->pw_dir;
+    string home = string(homedir);
     tool();
-    if (directory_exists("/usr/share/figlet/fonts") == 10) {
-        system("sudo cp Bloody.flf /usr/share/figlet/fonts");
-    }
-    else {
-        system("sudo cp Bloody.flf /usr/share/figlet/");
-    }
     system("clear");
     system("figlet -c -f Bloody 'Munseer' | lolcat");
-    if (file_exists("/usr/local/bin/manager") == 1) {
-        cout << "Script Already Installed" << endl;
-        exit(0);
-        // cout << "if executed\n";
-        // cout << file_exists("/usr/local/bin/manager");
+    if (file_exists("/usr/local/bin/manager") != 1) {
+        cout << "Installing Script\n";
+        sleep(1);
+        system("sudo cp manager.py /usr/local/bin/manager");
+        cout << "Creating Required Directories\n";
+        sleep(0.9);
+        if (directory_exists("/usr/share/figlet/fonts/") != 0) {
+            system("sudo cp Bloody.flf /usr/share/figlet/fonts");
+        }
+        else {
+            system("sudo cp Bloody.flf /usr/share/figlet/");
+        }
+        if (directory_exists(home+"/.config/manager/") != 1) {
+            filesystem::create_directory(home+"/.config/manager");
+        }
+        if (directory_exists(home+"/.config/manager/log/") != 1) {
+            filesystem::create_directory(home+"/.config/manager/log/");
+        }
+        if (directory_exists(home+"/.config/manager/backup/") != 1) {
+            filesystem::create_directory(home+"/.config/manager/backup/");
+        }
+        cout << "Moving Files\n";
+        sleep(0.5);
+        if (file_exists(home+"/.config/manager/config.py") != 1) {
+            filesystem::copy("config.py", home+"/.config/manager/");
+        }
+        else {
+            cout << "Found Existing Configuration\n";
+        }
+        if (file_exists(home+"/.config/manager/db.sqlite3") != 1) {
+            system("touch ~/.config/manager/db.sqlite3");
+        }
+        else {
+            cout << "Found Existing Database\n";
+        }
+        sleep(0.4);
+        if (file_exists(home+"/.config/manager/menu.py") != 1) {
+            filesystem::copy("menu.py", home+"/.config/manager/");
+        }
+        cout << "To run the script type 'manager'" << endl;
     }
     else {
-        // cout << "Installing Script" << endl;
-        cout << file_exists("/usr/local/bin/manager");
+        cout << "Script Already Installed" << endl;
+        exit(1);
     }
 }
