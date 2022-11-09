8b32c6d124d16a068ddc6d8207d514183cbd7b0e
Munseer-am <munnamunseer7@gmail.com>
2022-11-09 19:59:28 +0530

    Fixed Typo

--- manager.py
+++ manager.py
@@ -32,7 +32,7 @@ except ImportError:
 parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--reset", help="reset the password of script", action="store_true")
 parser.add_argument("-U", "--uninstall", help="Delete script file from /usr/local/bin/", action="store_true")
-parser.add_argument("--REMOVE", help="Delete all files and directories related password manager", action="store_true")
+parser.add_argument("--REMOVE", help="Delete all files and directories related to password manager", action="store_true")
 args = parser.parse_args()
 
 start = time()
