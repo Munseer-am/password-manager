a4b8cdd302e0fd00d9224d4f1b6a92ab939584e1
Munseer-am <munnamunseer7@gmail.com>
2022-11-13 12:18:32 +0530

    Changed to f-string

--- setup.py
+++ setup.py
@@ -69,33 +69,33 @@ class Main:
 			print("Installing Script")
 			os.system("sudo cp manager.py /usr/local/bin/manager && sudo chmod +rwx /usr/local/bin/manager")
 			print("Creating required directories")
-			if self.exists(self.home+"/.config/manager") != True:
-				os.mkdir(self.home+"/.config/manager/")
-			if self.exists(self.home+"/.config/manager/log/") != True:
-				os.mkdir(self.home+"/.config/manager/log/")
-			if self.exists(self.home+"/.config/manager/backup/") != True:
-				os.mkdir(self.home+"/.config/manager/backup/")
+			if self.exists(f"{self.home}/.config/manager") != True:
+				os.mkdir(f"{self.home}/.config/manager/")
+			if self.exists(f"{self.home}/.config/manager/log/") != True:
+				os.mkdir(f"{self.home}/.config/manager/log/")
+			if self.exists(f"{self.home}/.config/manager/backup/") != True:
+				os.mkdir(f"{self.home}/.config/manager/backup/")
 			print("Moving Files")
-			if self.exists(self.home+"/.config/manager/config.py"):
+			if self.exists(f"{self.home}/.config/manager/config.py"):
 				print("Found Existing Configuration")
 			else:
-				shutil.copy("lib/config.py", self.home+"/.config/manager/config.py")
-			if self.exists(self.home+"/.config/manager/db.sqlite3"):
+				shutil.copy("lib/config.py", f"{self.home}/.config/manager/config.py")
+			if self.exists(f"{self.home}/.config/manager/db.sqlite3"):
 				print("Found Existing Database")
-			if self.exists(self.home+"/.config/manager/menu.py") != True:
-				shutil.copy("lib/menu.py", self.home+"/.config/manager/menu.py")
-			if self.exists(self.home+"/.config/manager/insults.py") != True:
-				shutil.copy("lib/insults.py", self.home+"/.config/manager/insults.py")
+			if self.exists(f"{self.home}/.config/manager/menu.py") != True:
+				shutil.copy("lib/menu.py", f"{self.home}/.config/manager/menu.py")
+			if self.exists(f"{self.home}/.config/manager/insults.py") != True:
+				shutil.copy("lib/insults.py", f"{self.home}/.config/manager/insults.py")
 			if self.exists("/usr/local/bin/manager_repair") != True:
 				os.system("sudo cp setup.py /usr/local/bin/manager_repair && sudo chmod +rwx /usr/local/bin/manager_repair")
 			print("TO RUN THE SCRIPT TYPE `manager`")
 		else:
-			if self.exists(self.home+"/.config/manager/config.py") != 1:
-				shutil.copy("lib/config.py", self.home+"/.config/manager/config.py")
-			if self.exists(self.home+"/.config/manager/menu.py") != True:
-				shutil.copy("lib/menu.py", self.home+"/.config/manager/menu.py")
-			if self.exists(self.home+"/.config/manager/insults.py") != True:
-				shutil.copy("lib/insults.py", self.home+"/.config/manager/insults.py")
+			if self.exists(f"{self.home}/.config/manager/config.py") != 1:
+				shutil.copy("lib/config.py", f"{self.home}/.config/manager/config.py")
+			if self.exists(f"{self.home}/.config/manager/menu.py") != True:
+				shutil.copy("lib/menu.py", f"{self.home}/.config/manager/menu.py")
+			if self.exists(f"{self.home}/.config/manager/insults.py") != True:
+				shutil.copy("lib/insults.py", f"{self.home}/.config/manager/insults.py")
 			if self.exists("/usr/local/bin/manager_repair") != True:
 				os.system("sudo cp setup.py /usr/local/bin/manager_repair && sudo chmod +rwx /usr/local/bin/manager_repair")
 			print("Script Already Installed")
