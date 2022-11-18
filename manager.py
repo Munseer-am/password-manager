09822616e21184fbe3b007d61b86aa7734a20b00
Munseer-am <munnamunseer7@gmail.com>
2022-11-18 20:22:36 +0530

    Reduced Script Size

---
+++ manager.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+import argparse
+import os
+import time
+import sys
+
+try:
+    sys.path.insert(0, f"{os.path.expanduser('~')}/.config/manager/")
+    import essentials
+except ImportError:
+    os.system("sudo rm /usr/local/bin/manager")
+    os.system("manager_repair")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--reset", help="reset the password of script", action="store_true")
+parser.add_argument("-U", "--uninstall", help="Delete script file from /usr/local/bin/", action="store_true")
+parser.add_argument("--REMOVE", help="Delete all files and directories related to password manager",
+                    action="store_true")
+args = parser.parse_args()
+
+start = time.time()
+
+
+class Main(essentials.Main):
+    def __init__(self):
+        super(Main, self).__init__()
+        if args.reset:
+            self.reset()
+        elif args.REMOVE:
+            self.delete()
+        elif args.uninstall:
+            essentials.uninstall_script()
+        else:
+            self.security()
+
+
+try:
+    if __name__ == "__main__":
+        Main()
+    end = time.time()
+    print(f"Execution Time: {end-start}")
+    time.sleep(4)
+    os.system("clear")
+except KeyboardInterrupt:
+    os.system("clear")
+    exit(0)
