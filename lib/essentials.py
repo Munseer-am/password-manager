a0ae2415c4fa14fd96bc4e9aae2162400571afcd
Munseer-am <munnamunseer7@gmail.com>
2022-12-30 15:24:21 +0530

    Removed version check

--- lib/essentials.py
+++ lib/essentials.py
@@ -17,7 +17,6 @@ from string import ascii_lowercase, ascii_uppercase, digits
 from threading import Thread
 
 __author__ = "Munseer-am"
-__version__ = "1.0.1"
 
 try:
     sys.path.insert(0, f"{os.path.expanduser('~')}/.config/manager")
