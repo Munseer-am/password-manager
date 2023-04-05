e6c879418ed7448228b733ab1a7017a0f1945635
Munseer-am <munnamunseer7@gmail.com>
2023-04-05 21:43:54 +0530

    print author's name

--- lib/essentials.py
+++ lib/essentials.py
@@ -6,6 +6,7 @@ import os
 import re
 import sqlite3
 import sys
+import time
 from cryptography.fernet import Fernet
 from functools import cache
 from hashlib import sha256
@@ -38,6 +39,20 @@ def generate_password():
     all_chars = ascii_uppercase + ascii_lowercase + sym + digits
     password = "".join(sample(all_chars, 20))
     return password
+
+def sub_print(text, leading_spaces = 0):
+    text_chars = list(text)
+    current, mutated = '', ''
+    for i in range(len(text)):            
+        original = text_chars[i]
+        current += original
+        mutated += f'\033[1;38;5;82m{text_chars[i].upper()}\033[0m'
+        print(f'\r{" " * leading_spaces}{mutated}', end = '')
+        time.sleep(0.05)
+        print(f'\r{" " * leading_spaces}{current}', end = '')
+        mutated = current
+
+    print(f'\r{" " * leading_spaces}{text}\n')
 
 
 def sha256_encoder(word: str):
